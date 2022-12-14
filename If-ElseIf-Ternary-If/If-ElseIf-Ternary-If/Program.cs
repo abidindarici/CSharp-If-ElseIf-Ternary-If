33b25a5c983251fc0b2470031133d5947ba00566
Abidin Darıcı <114923176+abidindarici@users.noreply.github.com>
2022-12-14 13:08:16 +0300

    Add files via upload

---
+++ If-ElseIf-Ternary-If/If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /* //İLK AŞAMA SAATİN KAÇ 6 DAN ÖNCE YA DA SONRA OLDUĞUNU SORGULATIP SONRA İYİ GÜNLER YA DA İYİ GECELER DEDİRİYORUZ.
+        
+        int time = DateTime.Now.Hour;
+
+        if(time <=18)
+            Console.WriteLine("İYİ GÜNLER");
+        else
+            Console.WriteLine("İYİ GECELER");
+
+        Console.ReadKey();
+        */
+
+
+        int time = DateTime.Now.Hour;
+
+        if (time >= 6 && time <=11)
+            Console.WriteLine("GÜNAYDIN");
+        else if (time <=18)
+            Console.WriteLine("İYİ GÜNLER");
+        else
+            Console.WriteLine("İYİ GECELER");
+
+        
+        //BU ALANDA İF ELSE İN FARKLI BİR ŞEKİLDE YAPIMINI GÖRÜYORUZ.
+        string sonuc = time<= 18 ? "İyi Günler!" : "İyi Geceler!";
+        
+        sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+        Console.ReadKey();
+    }
+}
